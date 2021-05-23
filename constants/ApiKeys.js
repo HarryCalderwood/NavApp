b55d70420d92c6573fc059c35be7a26a85f17926
Harry Calderwood <73143902+HarryCalderwood@users.noreply.github.com>
2021-05-23 21:38:52 +0100

    Update ApiKeys.js

--- constants/ApiKeys.js
+++ constants/ApiKeys.js
@@ -1,15 +1,14 @@
 export default {
-
-    firebaseConfig: {
-        apiKey: "AIzaSyA3eI1wlFWTri8gBkg4LrJpnUny7_wwi7c",
-        authDomain: "desk-officer.firebaseapp.com",
-        databaseURL: "https://desk-officer-default-rtdb.firebaseio.com",
-        projectId: "desk-officer",
-        storageBucket: "desk-officer.appspot.com",
-        messagingSenderId: "615037685218",
-        appId: "1:615037685218:web:e90406ffa14b6d8a6ffdbb",
-        measurementId: "G-XJ4P0TJKFW"
-    }
-}
+  firebaseConfig: {
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "dprojectID",
+    storageBucket: "storageBucket",
+    messagingSenderId: "senderID",
+    appId: "appID",
+    measurementId: "measurementID",
+  },
+};
 
 
